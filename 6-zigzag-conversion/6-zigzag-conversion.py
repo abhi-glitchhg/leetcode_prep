4ac8a8e240a6976bf36ae879d9997fc81b40cdf3
glitchhg <104071715+glitchhg@users.noreply.github.com>
2022-08-06 13:08:33 +0530

    Time: 102 ms (47.49%), Space: 14 MB (51.46%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,26 +1,26 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         
-        if numRows ==1:
+        if numRows == 1:
             return s
         res = [""]* numRows
         res[0] = s[0]
         
-        print(res,len(res))
+        #print(res,len(res))
         j = 0 
         i = 1
         while (i<len(s)):
             for j in range(1,numRows):
                 if i == len(s):
                     break
-                print(i,j)
+                #print(i,j)
                 res[j] += s[i]
                 i+=1
                 
             for j in range(numRows-2,-1,-1):
                 if i == len(s):
                     break
-                print(i,j)
+                #print(i,j)
                 res[j] += s[i]
                 i+=1
         ans=""
