3f24d0654c5a4891a7a2e7a361be384cdd04bd75
glitchhg <104071715+glitchhg@users.noreply.github.com>
2022-08-06 12:39:40 +0530

    Time: 19 ms (67.73%), Space: 11 MB (33.54%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,9 +1,43 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            if target-nums[i]  in nums:
-                ans= nums.index(target - nums[i])
-                if ans!=i:
-                    ans =[i,ans]
-                    break
-        return ans
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        // two pointer approach
+        vector<int> sol ;
+        
+        vector<pair<int,int> > mapping;
+        for (int i=0; i<nums.size();i++){
+        mapping.push_back({nums[i],i});
+        }
+        
+        int pointer1 = 0;
+        int pointer2=  nums.size()-1;
+        
+        int n = sizeof(nums) / sizeof(nums[0]);
+        
+        sort(mapping.begin(),mapping.end());
+        
+        while (pointer2>pointer1){
+            if (mapping[pointer1].first+mapping[pointer2].first>target){
+                pointer2--;
+            }
+            else if (mapping[pointer1].first+mapping[pointer2].first<target){
+                pointer1++;
+            }
+            else{
+                
+                sol.push_back(mapping[pointer1].second);
+                sol.push_back(mapping[pointer2].second);
+            
+                break;
+                
+            }
+        }
+        
+        
+        return sol;
+            
+        
+    }
+        
+};
