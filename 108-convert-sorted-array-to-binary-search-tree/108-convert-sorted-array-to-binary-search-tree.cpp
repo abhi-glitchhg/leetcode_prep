e2e005ea38fdb5d7bc6c6c80c28f87c64934c6f9
glitchhg <104071715+glitchhg@users.noreply.github.com>
2022-08-20 23:29:23 +0530

    Time: 34 ms (17.75%), Space: 23.6 MB (20.14%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.cpp
@@ -15,22 +15,11 @@ public:
         int end = nums.size();
         int half_size=  end/2;
         int start = 0;
-        int to_start=0;
-        //if (end%2 !=0)
-        to_start=1;
         if(end==1){
             TreeNode* node = new TreeNode(nums[0]);
-    
-           // cout << "this is base condition :"<< nums[0]<<endl;
             return node;
-            
             }
-        
-        
          else if (end == 2){
-            
-            
-            
             TreeNode* node0 = new TreeNode(nums[0]);
             TreeNode* node = new TreeNode(nums[1],node0,NULL );
             return node;
@@ -42,17 +31,7 @@ public:
         auto first_end = nums.cbegin() + half_size;
         auto last = nums.cend();
             vector<int> beg(first,first_end);
-            vector<int> las(first_end+to_start ,last);
-            cout << "this is out first half of the inputs"<<endl;
-            for (int i: beg)
-                cout<<i<<" ";
-            cout<<endl;
-            cout<<"this is second half"<<endl;
-            for (int i: las)
-                cout<<i<<" ";
-            cout<<endl;
-            
-            
+            vector<int> las(first_end+1 ,last);            
             TreeNode* new_node = new TreeNode(nums[half_size], sortedArrayToBST(beg), sortedArrayToBST(las));
             return new_node;
         }
