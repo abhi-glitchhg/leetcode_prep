ee0be8672c4d1364731ac144a6db1bd4933e5d3e
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-21 15:46:49 +0530

    Time: 8 ms (68.08%), Space: 13.1 MB (6.42%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        vector<int> lst;
+        travel(root, lst);
+        
+        //for (int i:lst){
+         //   cout << i <<" ";
+       // }
+        //cout << endl;
+        treen(root, 0, lst);
+        
+    }
+      
+    void travel(TreeNode* node,vector<int>& lst){
+        if  (node ==NULL) return ;
+        lst.push_back(node->val);
+        travel(node->left,lst);
+        travel(node->right, lst);
+    }
+    
+    void treen(TreeNode* node, int i, vector<int>& lst){
+        if ((node == NULL) || i== lst.size())
+            return ;
+        node->val = lst[i];
+        //cout << node->val << " ";
+        i++;
+        node-> left = NULL;
+        node->right = NULL;
+        if (i!=lst.size()){ 
+            TreeNode* node2 = new TreeNode(lst[i]);
+            node->right = node2;
+            treen(node2, i, lst);
+        }
+        
+    }
+};
