89d3307d3fcbab1af95e9d11e2cec693cc4bd63e
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-23 00:17:55 +0530

    Time: 27 ms (7.33%), Space: 6.9 MB (74.74%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> map1;
+        map<char, char> map2;
+        for (int i=0;i<t.size(); i++){
+            if (map1.find(s[i]) == map1.end() && map2.find(t[i]) == map2.end()){ 
+                map1[s[i]] = t[i];
+                map2[t[i]] = s[i];
+            }
+            else if (map1.find(s[i]) == map1.end() || map2.find(t[i]) == map2.end())
+                return false;
+            else if (map1[s[i]] != t[i] || map2[t[i] !=s[i]] )
+                return false;   
+        }
+        
+        return true;
+    }
+};
