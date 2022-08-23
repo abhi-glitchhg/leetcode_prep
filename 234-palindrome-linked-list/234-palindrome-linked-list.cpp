7233592e1e791ae107e379fbb3e21d8144d8e820
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-23 23:28:53 +0530

    Time: 265 ms (87.23%), Space: 123.4 MB (22.87%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> stack_obj; 
+        if (!head->next) return head;
+        ListNode* copyhead = head;
+        stack_obj.push(head->val);
+        head = head->next;
+        while (head){
+            stack_obj.push(head->val);
+            head = head->next;
+        }
+        head = copyhead;
+        while (!stack_obj.empty() && head){
+            if (stack_obj.top() != head->val) return false;
+            stack_obj.pop();
+            head = head->next;
+        }
+        return true;
+        
+    }
+};
