8c607a5999037f5cf6daaae7676b705e3307b63a
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-26 21:54:04 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (29.94%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -9,26 +9,22 @@ public:
             lolmap[gg]++; // at this point we have a count map for the given integer
         
         
-        vector<int> maps;
+       map<int, map<char,int>> OGmap;
+                
         for (int cow =0; cow<30;cow++){ 
             int i =(int) pow(2,cow);
             //cout << i << " ";
             string b = to_string(i);
-            if (b.size() == true_size)    
-                maps.push_back(i);
+            if (b.size() == true_size){ 
+            for (char j: b)
+                OGmap[i][j]++;
+                }
             
             else if (b.size()>x.size())
                 break;
         } 
         
         // here we only select the integers which have same size as the given number and hence reducing our search space.
-        map<int, map<char,int>> OGmap;
-        for (int x: maps){ 
-            string b = to_string(x);
-            for (char j: b)
-                OGmap[x][j]++;
-        }
-        
         int ans = 0;
         int counter=0;
         for (const auto f:OGmap){
@@ -37,18 +33,10 @@ public:
             for (auto const a:f.second){
                 if (lolmap[a.first] != a.second){ 
                     counter = 0;
-                    //cout << a.first << a.second << lolmap[a.first];
-                    //cout << "IM broke" <<endl;
                     break;}
                 else{
-                    counter+= a.second;}
-                }
-            
-
-            if (counter==true_size)               return true;
-            
-            //cout<<endl << counter << lolmap.size() <<endl;
-        }
+                    counter+= a.second;}}
+            if (counter==true_size)  return true;}
         return false;
         
         
