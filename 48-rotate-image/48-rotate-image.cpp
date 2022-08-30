4b4aa4da1b0a5d622249088c70c6b8965c3dacd3
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-30 21:15:41 +0530

    Time: 7 ms (17.34%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,12 +5,14 @@ public:
         for (int i=0;i<n-1;i++){
             for (int j =0;j<n-i-1;j++){
                 cout << i <<" "<< j << endl;
+                
                 int temp;
                 temp = matrix[i][j];
                 matrix[i][j] =matrix[n-1-j][n-1-i] ;
                 matrix[n-1-j][n-1-i] = temp;
             }
         }
+        
         reverse(matrix.begin(), matrix.end());
     }
 };
