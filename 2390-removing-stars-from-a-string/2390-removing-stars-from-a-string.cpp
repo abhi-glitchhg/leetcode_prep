c82287891aa95a572fdfd7f6db5570d74aac5561
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-31 00:15:35 +0530

    Time: 111 ms (71.47%), Space: 24 MB (97.86%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,29 +1,15 @@
+// clever approach i saw in the discussion way better than stack approach
+
 class Solution {
 public:
     string removeStars(string s) {
-        if (s.find("*")>s.length()) return s;
-        stack<char> q;
-        for (char a:s){
-            //if (q.empty() && a!='*') q.push(a);
-             if ( a=='*'){
-                q.pop();
-                //cout<<"hello1";
-                
-            }
-            else
-                q.push(a);
-             //cout<<a<<endl;
+        int j = 0;
+        for(auto c: s){
+          //  cout<< c << " "<< j<< endl;
+            if (c == '*') j--;
+            else s[j++] = c;
         }
-      //  cout<<"hello";
-        string f ="";
-        while (! q.empty()){
-            f= q.top()+f;
-          //  cout <<q.top() << endl;
-            q.pop();
-            
-        }
-            
-        return f;
         
+        return s.substr(0, j);
     }
 };
