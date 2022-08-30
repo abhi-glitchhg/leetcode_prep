b97160e016f1004a62c6f5349ebecdea88fa6987
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-30 21:03:51 +0530

    Time: 62 ms (13.31%), Space: 23.3 MB (13.87%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if ((p->val > root->val  && q->val<root->val)  || (p->val < root->val  && q->val > root->val)  ) return root;
+        else if (p->val == root->val || q->val == root->val) return root;
+        else if (p->val > root->val && q->val > root->val) root=root->right;
+        else root=root->left;
+        return lowestCommonAncestor(root,p,q);
+    }
+};
