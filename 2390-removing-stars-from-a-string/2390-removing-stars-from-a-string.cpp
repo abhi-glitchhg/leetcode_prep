57d312c6c1aa9dfc951327de6857c7817d2dfc15
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-31 00:02:17 +0530

    Time: 2712 ms (5.01%), Space: 684.7 MB (5.01%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        if (s.find("*")>s.length()) return s;
+        stack<char> q;
+        for (char a:s){
+            if (q.empty() && a!='*') q.push(a);
+            else if (q.top() != '*' && a=='*'){
+                q.pop();
+                //cout<<"hello1";
+                
+            }
+            else
+                q.push(a);
+             //cout<<a<<endl;
+        }
+      //  cout<<"hello";
+        string f ="";
+        while (! q.empty()){
+            f= q.top()+f;
+          //  cout <<q.top() << endl;
+            q.pop();
+            
+        }
+            
+        return f;
+        
+    }
+};
