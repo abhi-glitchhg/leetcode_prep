5013b7cbc32b8a232621e0f9e0858f34adedaf71
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-31 20:06:33 +0530

    Time: 430 ms (16.10%), Space: 141 MB (18.29%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -18,75 +18,37 @@ public:
         string sol ="";
         vector<char> path;
         bool a = travel(root,startValue,pp);
-        bool b =travelwithpath(root, destValue,qq,path,-1);
-        //for (int i:pp)
-        //cout <<i<< " ";
-        //cout<<endl;
-        //for (int i:qq)
-        //cout <<i<< " ";
+        bool b =travelwithpath(root, destValue,qq,path,'_');
+
          int min_l = min(pp.size(), qq.size());
         
         
-     if (find(pp.begin(), pp.end(), destValue) !=pp.end() || find(qq.begin(), qq.end(), startValue) != qq.end()) { 
-         if (pp.size()> qq.size()) for (int i=0;i<pp.size() - qq.size();i++) sol+="U";
-         else{
+     if (find(pp.begin(), pp.end(), destValue) !=pp.end() || find(qq.begin(), qq.end(), startValue) != qq.end()) { //if one of the node is parent of the other then we will travel only in one direction, ie. only up or only down(i.e R and L)
+         if (pp.size()> qq.size()) for (int i=0;i<pp.size() - qq.size();i++) sol+="U"; //this handles when the destVal is parent of startVal
+         else{ // when startVal is parent of destVal
              reverse(path.begin(), path.end());
-             //path.pop_back();
-              //   reverse(path.begin(), path.end());
-          //    for  (char a:path)
-          //       cout<<a<<endl;
              for (int i=0;i<qq.size()-pp.size();i++)
                  sol+=path[i];
              reverse(sol.begin(), sol.end());
          }
        }
         
-    else{     
-   // while (ppc<pp.size() && ppc<qq.size()){
-   //     if (pp[ppc] ==qq[ppc]) ppc++;
-   //     cout << pp[ppc] << qq[ppc]<<endl;
-    //}
-      //  cout << min_l<<endl;
+    else{     //none of the node are parent of each other; so we travel up till we find the first common ancestor and then we go down; in this down path we use the stored path in the travelwithpath function.
     int g=0;
     for (int i=0;i<min_l-1;i++)
         if (pp[i+1]==qq[i+1])
             g+=1;
-      //  for (int i:pp)
-       // cout <<i<< " "<<g<<endl;
-        
-    //cout <<endl;
-    //for (int i:qq)
-      //  cout <<i<< " ";
-    //cout <<endl;
-    //cout <<"stay string bro"<< g<<" ";
-    
     for (int i=g;i<pp.size()-1;i++)
         sol+="U";
     for (int i=g+1;i<qq.size();i++)
         sol+=path[i];
     }
-        //for (char c: sol)
-            //cout << c;
      return sol;   
-
     }
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    bool travel (TreeNode* head, int p, vector<int>& var){
+    bool travel (TreeNode* head, int p, vector<int>& var){ // a travel function which stores the path of the node into variable var;
         if (head == NULL ) return false;
         var.push_back(head->val);
         if (head->val == p)
@@ -99,7 +61,7 @@ public:
     
     }
     
-        bool travelwithpath (TreeNode* head, int p, vector<int>& var, vector<char>& path,char a){
+        bool travelwithpath (TreeNode* head, int p, vector<int>& var, vector<char>& path,char a){ //same as travel but now we are storing if we are going left or right into path vector.
         if (head == NULL ) return false;
         path.push_back(a);
         var.push_back(head->val);
