13200bd3bc5054c3511748c3dc374dc20a8d3bd4
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-08-31 13:10:00 +0530

    Time: 8 ms (69.54%), Space: 13.8 MB (48.94%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+       int max_size = cost.size();
+       vector<int>  v(cost.size());
+        for (int i =0;i<cost.size();i++)
+            v[i]=-1;
+       return min(travel(0,cost, v),travel(1,cost,v)) ;
+    }    
+    int travel(int index, vector<int>& cost,vector<int>& v){ 
+        if (index==cost.size()-2 || index==cost.size()-1) return cost[index];
+        if (v[index]==-1)
+            v[index] = cost[index] + min(travel(index+1, cost,v), travel(index+2, cost,v));
+        return v[index];
+    }
+    
+};
