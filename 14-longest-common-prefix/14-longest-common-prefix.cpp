4b0d50afb97cbd02b1f540e5472154b8b3e3c79f
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-16 00:35:16 +0530

    Time: 14 ms (13.86%), Space: 9.6 MB (15.06%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -9,10 +9,10 @@ public:
         if (min_len ==0) return "";
         string first = strs[0];
         string common = string(first.begin(), first.begin() + min_len);
-        
+        // here first we are finding the smallest string in the list of strs. helps in optimisation.
         for (int i=1; i<strs.size(); i++){
             string tocompare = strs[i];
-            
+            // here we are dealing with min_len only no string operations; faster imo. 
             for (int j=0;j<min_len;j++) {if ( tocompare[j] != common[j]) min_len = j; if (min_len == 0 ) return "";}
         }
         
