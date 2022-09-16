69205605484d0f140483f713aee2e39b94c34ad3
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-16 19:42:13 +0530

    Time: 5 ms (20.01%), Space: 6.4 MB (51.80%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -3,28 +3,12 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<int> ans;
         int i=0;
-        int gg = rowIndex;
-        // n-i / i +1 formula for nc(i+1)/nci jee adv PNC
-        
-        if (rowIndex==0)
-            ans.push_back(1);
-        else {
-            if (rowIndex%2 == 1) gg++;
-            ans.push_back(1);
-            long int jj = 1;
-            for (i; i< (gg/2) ; i++){
-                
-                jj = jj * (rowIndex-i)/(i+1);
-                ans.push_back((int)jj);
-                ///cout<<jj<< " ";
+        ans.push_back(1);
+        long int jj = 1;
+        for (i; i< rowIndex ; i++){
+            jj = jj * (rowIndex-i)/(i+1);
+            ans.push_back((int)jj);
             }
-            //cout<<i;
-            if (rowIndex%2==1) i--;
-            i--;
-            for (;i>-1;i--){
-                ans.push_back(ans[i]);
-            }
-        }
         return ans;
         
     }
