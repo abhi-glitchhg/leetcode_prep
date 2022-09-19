635006cacec2ba3d20f8d65174486f686f0093b2
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-19 23:14:13 +0530

    Time: 16 ms (5.14%), Space: 10.8 MB (32.45%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -12,22 +12,20 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int k) {
      int counter = 0;
-        if (head->next == NULL) return NULL;
         ListNode* oghead =head;
         while (head!=NULL){
          head = head->next;
-            counter++;
-     }
+            counter++;}
         
         head = oghead;
         
-        if (k == counter ) return head->next;
+       if (k == counter ) return head->next;
        while (counter!=k+1){
            head= head->next;
            counter--;
        }
         if (k == 1 ) head->next = NULL;
-        else    head->next = head->next->next;
+        else head->next = head->next->next;
     return oghead;    
     }
     
