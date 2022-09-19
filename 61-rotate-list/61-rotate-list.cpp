828fef82124e98ac9f280578bd527f23b55636d9
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-19 19:05:46 +0530

    Time: 11 ms (67.60%), Space: 11.7 MB (55.17%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+        vector<int> a;
+        if (head == NULL) return head;
+        ListNode* ogHead = head;
+        while(head!= NULL){
+            a.push_back(head->val);
+            head = head->next;
+        }
+        int n= a.size();
+        k = k % n;
+        head= ogHead;
+        for (int i =n-k;i<n;i++){
+            head->val = a[i];
+            head=head->next;
+        }
+         
+        for(int i=0;i<n-k;i++){
+            head->val = a[i];
+            head=head->next;
+        }
+        return ogHead;
+    }
+};
