ac883ade5f9ea0b9a0cc660d875e74a14aeb6e3f
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-25 18:27:42 +0530

    Time: 87 ms (18.86%), Space: 89.3 MB (87.16%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,24 +1,62 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        map<int,int> gg;
-        //for (auto i: nums1)
-        //    gg[i]++;
-        //for (auto i: nums2)
-         //   gg[i]++;
-        //sort gg wrt values
-        //
-        for (auto k:nums2)
-            nums1.push_back(k);
-        sort(nums1.begin(),nums1.end());
-        double sol;
-        if (nums1.size()%2 ==0) 
-             sol =((double)nums1[nums1.size()/2] + nums1[nums1.size()/2 -1] )/2;
-        else 
-             sol = nums1[nums1.size()/2];
         
+        int n = nums2.size();
+        int m = nums1.size();
         
-        //
-        return sol;
+        int cpp = 0;
+        int n_p = 0;
+        int m_p = 0;
+        int n1 =0;
+        int n2 =0;
+        while (cpp < (n+m)/2+1){
+            if (n_p < n && m_p < m){
+                if (nums1[m_p] > nums2[n_p] )
+                {
+                    if (cpp == (n+m)/2 -1)
+                        n1 = nums2[n_p];
+                    else if (cpp == (n+m)/2)
+                        n2 = nums2[n_p];
+                    n_p++;
+                }
+                else 
+                {
+                    if (cpp == (n+m)/2 -1)
+                        n1 = nums1[m_p];
+                    else if (cpp == (n+m)/2)
+                        n2 = nums1[m_p];
+                    m_p++;
+                }
+                    
+            }
+            
+            else if (n_p <n){
+                {
+                    if (cpp == (n+m)/2 -1)
+                        n1 = nums2[n_p];
+                    else if (cpp == (n+m)/2)
+                        n2 = nums2[n_p];
+                    n_p++;
+                }
+                
+            }
+            else     {
+                    if (cpp == (n+m)/2 -1)
+                        n1 = nums1[m_p];
+                    else if (cpp == (n+m)/2)
+                        n2 = nums1[m_p];
+                    m_p++;
+                }
+            cpp++;
+            
+        }
+        double ans = 0;
+       if ((n+m )%2 ==0 )
+           ans = (double) n1 + n2;
+        ans/=2;
+        if ((n+m )%2 ==1 )  ans = (double) n2;
+        //cout << n1 <<" "<< n2;
+        return ans;
     }
 };
