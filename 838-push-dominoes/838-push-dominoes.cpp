ff4b3c576f3c940856d99a8ed705daad3006f694
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-27 22:47:05 +0530

    Time: 34 ms (95.67%), Space: 15 MB (79.20%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        string sol = "";
+        for (int i=0;i<dominoes.size();i++){
+            if (dominoes[i] == '.'){
+                int point1 = i-1;
+                int point2 = i+1;
+                bool stop1= false;
+                bool stop2 = false;
+                while (true){
+                    //cout <<point1 <<" " << point2 <<endl;
+                    if (point1 < 0) stop1 = true;
+                    if (point2 >dominoes.size()-1) stop2= true; 
+                    if (!stop2) if (dominoes[point2] == 'R') stop2 = true;
+                    if (!stop1) if (dominoes[point1] == 'L') stop1 = true;
+                    
+                    if (stop1 && stop2) break;
+                    
+                    if (!stop1 && !stop2) if(dominoes[point2] == 'L' && dominoes[point1] == 'R') {sol+='.';break;}
+                     if (!stop2) if (dominoes[point2]=='L' && !stop2) {sol+='L';break;}
+                     if (!stop1) if (dominoes[point1]=='R' && !stop1) {sol+='R';break;}
+                    point1--;
+                    point2++;
+                }
+                
+            }
+            if (sol.size()!= i+1)
+                sol+= dominoes[i];
+        }
+        return sol;
+        
+    }
+};
