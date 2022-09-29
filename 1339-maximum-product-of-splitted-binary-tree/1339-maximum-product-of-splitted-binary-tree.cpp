62b78aeff2e210831c4fc26944c4ff8cbe2162d2
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-09-29 23:56:00 +0530

    Time: 151 ms (86.86%), Space: 77.4 MB (75.52%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,37 +1,23 @@
+
 class Solution {
 public:
-    int maxProduct(TreeNode* root) {
-     int sol=0;
-        unordered_map<TreeNode*, int> mapping;
-         sol=dfs(root, mapping);
-        queue<TreeNode*> q;
-        long int a = INT_MIN;
-        int total_sum = mapping[root];
-        q.push(root);
-        
-        while (!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            if (temp->left!=NULL) {
-                q.push(temp->left); 
-                if ( ((long int)(total_sum-mapping[temp->left])) * mapping[temp->left] > a) 
-                    a = (long int)(total_sum-mapping[temp->left]) * mapping[temp->left];
-                }
-            if (temp->right!=NULL) {
-                q.push(temp->right);
-                if ( ((long int)(total_sum-mapping[temp->right])) * mapping[temp->right] > a) 
-                    a = (long int)(total_sum-mapping[temp->right]) * mapping[temp->right];
-            }
-        }
-        a %= 1000000007;
-        return a;
-        
-        
+    int mod=1e9+7;
+    long long maxm;
+    int total=0;
+    int dfs(TreeNode* root)
+    {
+       if(!root) return 0;
+        int curr=dfs(root->left)+dfs(root->right)+root->val;
+        long long temp=curr*(long long)(total-curr);
+        maxm=max(maxm,temp);
+        return curr;
+
     }
-    
-    int dfs(TreeNode* head, unordered_map<TreeNode* , int> &mapping){
-        if (head==NULL) return 0;
+    int maxProduct(TreeNode* root) {
+        maxm=LLONG_MIN;
+        total=dfs(root);
+        dfs(root);
+        return maxm%mod;
         
-       return mapping[head]= head->val + dfs(head->left,mapping) + dfs(head->right, mapping);
     }
 };
