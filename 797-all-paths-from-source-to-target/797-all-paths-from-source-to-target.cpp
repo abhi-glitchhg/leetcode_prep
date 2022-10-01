fb9d6a69ebf900dee9d75a10da6ccc432df9ed63
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-01 22:20:44 +0530

    Time: 26 ms (52.79%), Space: 10.7 MB (82.01%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adc) {
+        //vector<int> visited(adc.size(), 0);
+        vector<vector<int>> ans;
+        vector<int>path;
+        path.push_back(0);
+        dfs(0,adc,ans,path);
+        return ans;
+        
+    }
+    
+    void dfs(int node, vector<vector<int>>&adjc, vector<vector<int>> &ans, vector<int> &path){
+        
+        //dfs in graph; add node while doing dfs on its children; when done remove from path;
+        
+        for (auto i: adjc[node]){
+                path.push_back(i);
+                if (i==adjc.size()-1) ans.push_back(path);
+                dfs(i, adjc,ans,path);
+                path.pop_back();
+            
+            
+        }
+        
+}
+};
