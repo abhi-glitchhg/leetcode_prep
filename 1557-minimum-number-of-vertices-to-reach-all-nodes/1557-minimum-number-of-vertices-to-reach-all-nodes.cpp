03770a464fd3b7f255c4705bf05332cfe63a4f32
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-06 19:18:45 +0530

    Time: 801 ms (12.08%), Space: 106.8 MB (25.29%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adjc(n);
+        //vector<vector<int>> adjc(n);
         vector<int> indegree(n,0);
         vector<int> ans;
         for (auto i : edges)
-        {
-            adjc[i[0]].push_back(i[1]);
             indegree[i[1]]++;
-        }
+        
         for (int i=0;i<n;i++) if (indegree[i]==0) ans.push_back(i);  
         return ans;
     }
