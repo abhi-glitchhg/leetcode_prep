5a69721cdc0329c86c1a7265f2cbeacd73887b5f
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-23 00:33:55 +0530

    Time: 164 ms (11.68%), Space: 39.4 MB (83.47%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -6,6 +6,7 @@ public:
         cur=0;res=0;
         
         for (auto i : values){
+            // prev best candidate was kept tracked; and then update it acc;
             
             res = max(cur+i, res);
             cur = max(cur, i) -1;
