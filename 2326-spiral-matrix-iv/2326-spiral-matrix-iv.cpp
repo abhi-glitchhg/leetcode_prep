bef4f65330579104973bcab35d9356cce46eb5e4
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-23 00:07:29 +0530

    Time: 327 ms (79.00%), Space: 127.9 MB (77.76%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.cpp
@@ -20,26 +20,18 @@ public:
         while (upper_row<=lower_row && left_column<=right_column){
             
             for (int i=left_column;i<=right_column;i++){
-    
-            if (head==NULL) to_be_added = -1;    
-            else {to_be_added =head->val; head= head->next;}
-                
-                //cout<< upper_row<< " "<< i<<" " << to_be_added <<"\n";
+                if (head==NULL) to_be_added = -1;    
+                else {to_be_added =head->val; head= head->next;}
                 ans[upper_row][i]= to_be_added;
-                            }
+            }
             upper_row+=1;
             
             if (upper_row>lower_row || left_column>right_column) break;
-            
-            
             for (int i=upper_row;i<=lower_row;i++){
             
                 if (head==NULL) to_be_added = -1;    
-            else {to_be_added =head->val; head= head->next;}
-                //cout<< i<<" "<<right_column<<" "<< to_be_added <<"\n";
+                else {to_be_added =head->val; head= head->next;}
                 ans[i][right_column]= to_be_added;
-            
-            
             }
             right_column--;
             if (upper_row>lower_row || left_column>right_column) break;
@@ -48,9 +40,9 @@ public:
             for (int i=right_column;i>=left_column;i--){
                 
                 if (head==NULL) to_be_added = -1;    
-            else {to_be_added =head->val; head= head->next;}
-                //cout<< lower_row<<" "<<i<<" "<<to_be_added <<"\n";
-                ans[lower_row][i]=to_be_added;}
+                else {to_be_added =head->val; head= head->next;}
+                ans[lower_row][i]=to_be_added;
+            }
 
             lower_row--;
             if (upper_row>lower_row || left_column>right_column) break;
@@ -59,12 +51,9 @@ public:
             for (int i= lower_row;i>=upper_row;i--){
          
                 if (head==NULL) to_be_added = -1;    
-            else {to_be_added =head->val; head= head->next;}
-                //cout<<i<<" ."<<left_column<< " "<< to_be_added<<"\n";
+                else {to_be_added =head->val; head= head->next;}
                 ans[i][left_column] = to_be_added;
-
-            
-        }
+            }
             left_column++;
         }
         return ans;
