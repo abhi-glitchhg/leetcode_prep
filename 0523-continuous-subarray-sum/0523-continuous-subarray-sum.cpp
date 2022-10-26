b93976cdda789548fa1b0a0584f9f966067b2618
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-26 23:55:10 +0530

    Time: 509 ms (38.90%), Space: 112.5 MB (92.34%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,3 +1,29 @@
+/*class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        // brute force O(n2) will give TLE for the 10^5 length;
+        if (nums.size()<2) return false;
+
+        unordered_map<int, int> mp;
+        int summ=0;
+        for (int i=0;i<k;i++)
+            mp[i]=-2;
+        mp[0]=-1;
+        for (int i=0;i<nums.size();i++){
+            summ+=nums[i];
+            if (mp[summ%k]==-2)
+                mp[summ%k] = i;  // here i was doing i
+            else if (mp[summ%k]<i-1) //sol suggest i and i+1 in above place but doenst matter; i have initialised as 0,-1 instead of 0,0
+                return true;
+        }
+        return false;
+    }
+};*/
+
+
+/// above sol gives TLE because of the matrix initialisation... use count method just gonna copy paste the solution here.
+
+
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
