5eb63d5e35c3c1f96995577d68c79205e6aacf90
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-10-29 15:30:41 +0530

    Time: 1298 ms (6.80%), Space: 17.6 MB (16.46%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -6,14 +6,13 @@ public:
         return canbevisited[nums.size()-1];
     }
     
-    
     void dp(int index, int num_jumps, vector<int> &canbevisited, vector<int> &nums){
         if (index>=canbevisited.size()) return;
         if (canbevisited[index] > num_jumps){
             canbevisited[index]=num_jumps;
-            for (int j=index+nums[index];j>index;j--){ 
-                //if(j>=nums.size()) break;// just effectively stops further calls in the for loop as we can have a test case with very high nums[index]
-                dp(j, 1+num_jumps,canbevisited, nums);}
+            for (int j=index+nums[index];j>index;j--)
+                dp(j, 1+num_jumps,canbevisited, nums);
+            
         }
         return;
     }
