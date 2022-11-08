1f41ca5a0214224ec99e42645c869c5fbbd5a1d4
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-09 01:04:56 +0530

    Time: 471 ms (61.62%), Space: 104.7 MB (62.09%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestCycle(vector<int>& edges) {
+        int sol=0; int counter=0;
+        int ans=-1;
+        vector<int> visited(edges.size(),0);
+        vector<int> dfsvisited(edges.size(),0);
+        for (int i=0;i<edges.size();i++){ 
+            bool domath=false;
+            sol = -1;
+            counter=0;
+            if (visited[i]==0)
+                dfs(i,edges,visited,dfsvisited,sol, domath,counter);
+                //cout<< counter<<" "<<sol<<" \n";} 
+            ans = max(ans,counter);
+        }
+        if (ans==0) return -1;
+            
+        return ans+1;
+    }
+    
+    void dfs(int index, vector<int> &edges, vector<int> &visited, vector<int> &dfsvisit, int &sol, bool &domath,int &counter){
+        
+        visited[index]=1;
+        dfsvisit[index]=  1; 
+        dfsvisit[index]=1;
+         int i = edges[index];
+        if (i!=-1) 
+             if (visited[i]==0)
+                dfs(i, edges,visited,dfsvisit,sol,domath,counter);
+        else 
+            if (visited[i]==1 && dfsvisit[i]==1)
+            {domath=true; sol = i;}
+        if (index==sol) domath=false;
+        if (domath) counter++;
+        dfsvisit[index]=0;
+        
+    }
+};
