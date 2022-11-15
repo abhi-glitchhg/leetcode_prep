4d239feea4d803fee7392557aeab1ff8347e068e
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-15 14:20:46 +0530

    Time: 483 ms (53.79%), Space: 102.8 MB (51.26%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.rbegin(),asteroids.rend());
-        vector<long long> sum_ast(asteroids.size());
-        long long summ=0;
-        for (int i=asteroids.size()-1; i>=0;i--){
-           summ+= asteroids[i];
-            sum_ast[i]=summ;
-        }
-        
-        int stop=asteroids.size();
+        sort(asteroids.begin(), asteroids.end());
         long long og_mass=mass;
-        
-        while(true){
-            bool flag=false;
-            int index=0;
-            while (index<stop){
-            
-            if (asteroids[index]<=og_mass){
-                og_mass= mass+ sum_ast[index];
-                flag=true;
-                stop=index;
+        for (auto i: asteroids){
+            if (og_mass<i) return false;
+            else {
+                og_mass+=i;
             }
-            index++;
-            if (og_mass>=asteroids[0]) break;
-            if (stop==0) break;
-            }
-            if (!flag) break; if (stop==0) break;
-            if (og_mass>=asteroids[0]) break;
-                    
         }
-        //cout<< og_mass<< " "<<stop<<" \n";
-        if (og_mass<asteroids[0]) return false;
         return true;
     }
 };
