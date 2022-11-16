058dffa67123ea1a3cf80dbf621333e87c069de2
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-16 22:54:00 +0530

    Time: 4 ms (39.87%), Space: 7 MB (74.49%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -8,9 +8,9 @@ public:
         while (nums.size()>0){
             
             int a= nums[0];
-            int b= nums[nums.size()-1];
+             a+= nums[nums.size()-1];
             nums.pop_back();nums.erase(nums.begin());
-            gg[a+b]++;
+            gg[a]++;
         }
         return gg.size();
     }
