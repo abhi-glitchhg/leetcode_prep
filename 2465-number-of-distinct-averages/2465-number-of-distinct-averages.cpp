2c4170f1d989ca43ee74066e9519563f90f5f92c
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-16 22:53:20 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (29.40%) - LeetHub

--- 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
+++ 2465-number-of-distinct-averages/2465-number-of-distinct-averages.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int distinctAverages(vector<int>& nums) {
-        unordered_map<double, int> gg;
+        unordered_map<int, int> gg;
         
         sort(nums.begin(),nums.end());
         
@@ -10,9 +10,7 @@ public:
             int a= nums[0];
             int b= nums[nums.size()-1];
             nums.pop_back();nums.erase(nums.begin());
-            double ans= ((double)a + b)/2;
-            //cout<<ans<<" ";
-            gg[ans] +=1;
+            gg[a+b]++;
         }
         return gg.size();
     }
