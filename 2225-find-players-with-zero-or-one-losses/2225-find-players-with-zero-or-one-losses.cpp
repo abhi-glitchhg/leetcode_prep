c20740c3d0a7cb7a4903020ade817e4e7cc6644c
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-28 21:15:45 +0530

    Time: 1746 ms (15.26%), Space: 194.5 MB (21.72%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -7,26 +7,21 @@ public:
        {
            winner[it[0]]++;
            loser[it[1] ]++;
+           
        }
         
         vector<vector<int>> ans(2);
-        unordered_map<int,int> visited;
         for (auto it:winner){
             if (loser[it.first]==0)
                 ans[0].push_back(it.first);
-             else if (loser[it.first]==1){ 
-                ans[1].push_back(it.first);
-                visited[it.first]=1;
-            }
         }
         
         for (auto it:loser)
-            if (it.second==1 && visited[it.first]==0){
-                visited[it.first]=1;
+            if (it.second==1){
                 ans[1].push_back(it.first);
             }
-        sort(ans[0].begin(),ans[0].end());
-        sort(ans[1].begin(),ans[1].end());
+        //sort(ans[0].begin(),ans[0].end());
+        //sort(ans[1].begin(),ans[1].end());
         return ans;
     }
 };
