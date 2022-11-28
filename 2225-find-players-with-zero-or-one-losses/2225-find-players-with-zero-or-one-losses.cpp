efc34847608ad1641ed1fd137c4e9f0ad36440ac
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-11-28 21:34:35 +0530

    Time: 1800 ms (12.24%), Space: 209.5 MB (10.08%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
        map<int,int>  winner;
-       map<int,int> loser;
+       unordered_map<int,int> loser;
        for (auto it: matches)
        {
-           winner[it[0]]++;
+           winner[it[0]];
+           winner[it[1]];
            loser[it[1] ]++;
-           
        }
         
         vector<vector<int>> ans(2);
         for (auto it:winner){
             if (loser[it.first]==0)
                 ans[0].push_back(it.first);
+            
+            else if (loser[it.first]==1)
+                ans[1].push_back(it.first);
         }
         
-        for (auto it:loser)
-            if (it.second==1){
-                ans[1].push_back(it.first);
-            }
+
         //sort(ans[0].begin(),ans[0].end());
         //sort(ans[1].begin(),ans[1].end());
         return ans;
