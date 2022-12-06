f2484367bf50167c1d6b7286b41390ae65aa21c9
Someone who is not good at DSA <104071715+glitchhg@users.noreply.github.com>
2022-12-07 00:14:24 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (11.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        reverse(s.begin(),s.end());
         stringstream ss(s);
         string b;
         string ans;
         while (getline(ss, b, ' ')){
             if (b!= string(b.size(),' '))
+            { 
                 ans=b;
+                break;
+            }
         }
         return ans.size();
     }
